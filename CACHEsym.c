46794aa13c63121c3a75014e310e547437ce8467
lgalonso <lucascardedal@gmail.com>
2020-12-04 13:11:15 +0100

    Archivo C para las funciones de traducción

---
+++ CACHEsym.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void traduccionAcceso(char);
+//Recibe una direccion de acceso y la descompone en bloque, linea, ETQ y palabra
+//Sabemos que los 3 ultimos bits son para la palabra, 2 para la linea y 5 para la ETQ
+void traduccionAcceso(char acceso){
+
+}
+
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   return 0;
+}
+
