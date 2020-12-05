3df3966039d46a518e8ff8b6d42f13d7d0639745
oscarbaltasar <oscar@Ordenador.localdomain>
2020-12-05 15:44:45 +0100

    funcion Inicio y Leer Linea de accesos_memoria.txt

--- CACHEsym.c
+++ CACHEsym.c
@@ -19,11 +19,26 @@ void inicio(unsigned char RAM[1024], T_LINEA_CACHE Cache[4]){
    //printf("ETQ: %d, Dato: %d", Cache[i].ETQ, Cache[i].Datos[j]);
   }
  }
+ fclose(LeerRAM);
+}
+
+void LeerLinea(char LineaLeida[5], int linea){
+ FILE *Linea;
+ Linea = fopen("accesos_memoria.txt", "r");
+ int i;
+ for(i=0; i<linea; i++){
+  fgets(LineaLeida, 5, Linea);
+  fgetc(Linea);
+  printf("%s", LineaLeida);
+ }
+ fclose(Linea);
 }
 
 void main(){
  unsigned char RAM[1024];
+ char Linea[5];
  T_LINEA_CACHE Cache[4];
  inicio(RAM, Cache);
  //printf("\n%s",RAM);
+ LeerLinea(Linea,3);
 }
