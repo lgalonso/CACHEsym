1c7e3a2d222242c849cf6702e442506570ff7de5
Ghiojo <ghiojo@hotmail.com>
2020-12-06 17:02:32 +0100

    Agregadas las funciones de impresión, comentarios.

---
+++ Test.c
@@ -0,0 +1,5 @@
+
+void main(){
+    int x = 42;
+    printf("%04X", x);
+}
--- main.c
+++ main.c
@@ -6,17 +6,31 @@ typedef struct {
     short int Datos[8];
 } T_LINEA_CACHE
 
+void printfail(int tiempo, int palabra, int linea, int etiqueta, int bloque, int acceso, int fallos){
+    printf("T:%d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X", tiempo, fallos, acceso, etiqueta, linea, palabra, bloque);
+}
+
+void printsuccess(int tiempo, int palabra, int linea, int etiqueta, int acceso, int dato){
+    printf("T:%d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X", tiempo, acceso, etiqueta, linea, palabra, dato);
+}
+
 //Busca la linea de la cache y compara las etiquetas, si la etiqueta está bién, devuelve
-int busqueda(T_LINEA_CACHE *cache, int palabra, int linea, int etiqueta){
+int busqueda(T_LINEA_CACHE *cache, int palabra, int linea, int etiqueta, int *tiempo, int *fallos, int acceso, int bloque){
 
     if(cache[linea].ETQ == etiqueta){
-        //Llama a Imprimir acierto
-        return 1
+        //Recoge el dato
+        dato = cache[linea].Datos[palabra];
+        //Llama a Imprimir Acierto
+        printsuccess(*tiempo, palabra, linea, etiqueta, acceso, dato);
+        return 1;
+        //Modificar Tiempo
     }
     else
     //Llama a Imprimir Fallo
+    printfail(*tiempo, palabra, linea, etiqueta, bloque, acceso, *fallos);
     //Llama a Cargar de la Ram a la Caché
-    return 0
+    //Modificar Tiempo
+    return 0;
 }
 
 
