3b4834308480ba2c88e98aae0b4a606126b2d19d
lgalonso <lucascardedal@gmail.com>
2020-12-06 17:07:00 +0100

    Integrados funciones traduccion, busqueda y prints

--- CACHEsym.c
+++ CACHEsym.c
@@ -48,6 +48,49 @@ int LeerLinea(char LineaLeida[5], int linea){
  return 1;
 }
 
+// Recibe una direccion de acceso y la guarda en bloque, linea, ETQ y palabra que se pasan por referencia
+// Sabemos que los 3 ultimos bits son para la palabra, 2 para la linea y 5 para la ETQ
+void traduccionAcceso(char* acceso, int* palabra, int* linea, int* etq, int* bloque){
+   printf("Traduciendo: %s...\n", acceso);
+
+   // Funcion que convierte la parte inicial de un string en un long int segun la base indicada
+   int num = (int)strtol(acceso, NULL, 16);
+
+   *palabra = num & 0x0007;
+   // Dividimos por un numero en base 2 para quedarnos con los bits que nos interesan
+   *linea = (num & 0x0018)/pow(2,3);
+   *etq = (num & 0x03E0)/pow(2,5);
+   *bloque = (num & 0x03F8)/pow(2,3);
+   
+}
+
+void printfail(int tiempo, int palabra, int linea, int etiqueta, int bloque, int acceso, int fallos){
+    printf("T:%d, Fallo de CACHE %d, ADDR %04X ETQ %X linea %02X palabra %02X bloque %02X", tiempo, fallos, acceso, etiqueta, linea, palabra, bloque);
+}
+
+void printsuccess(int tiempo, int palabra, int linea, int etiqueta, int acceso, int dato){
+    printf("T:%d, Acierto de CACHE, ADDR %04X ETQ %X linea %02X palabra %02X DATO %02X", tiempo, acceso, etiqueta, linea, palabra, dato);
+}
+
+//Busca la linea de la cache y compara las etiquetas, si la etiqueta está bién, devuelve
+int busqueda(T_LINEA_CACHE *cache, int palabra, int linea, int etiqueta, int *tiempo, int *fallos, int acceso, int bloque){
+
+    if(cache[linea].ETQ == etiqueta){
+        //Recoge el dato
+        dato = cache[linea].Datos[palabra];
+        //Llama a Imprimir Acierto
+        printsuccess(*tiempo, palabra, linea, etiqueta, acceso, dato);
+        return 1;
+        //Modificar Tiempo
+    }
+    else
+    //Llama a Imprimir Fallo
+    printfail(*tiempo, palabra, linea, etiqueta, bloque, acceso, *fallos);
+    //Llama a Cargar de la Ram a la Caché
+    //Modificar Tiempo
+    return 0;
+}
+
 void main(){
  unsigned char RAM[N];
  char Linea[5];
